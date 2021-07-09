713be577ed81453c41e504699d44afeec018aad6
ZeDon <fornownot@useratall.com>
2021-07-09 04:23:14 +0200

    Added verbosity and comments to StartFarmerSearchingInHomesDirectory.sh

--- StartFarmerSearchingInHomesDirectory.sh
+++ StartFarmerSearchingInHomesDirectory.sh
@@ -3,6 +3,8 @@
 # If found, tries to deduce the command needed to start the farmer and starts it (killing previous daemon pid)
 # TODO: Checks for availability of commands and present user errors.
 
+# Usage: Execute from root changing {homePath} var if needed, can be added in crontab or init systems or whatever
+
 homePath="/home"
 availableChains=$(find "$homePath" -mindepth 3 -maxdepth 3 -name "activate" -type l)
 echo "$availableChains" | while read availableChain
@@ -16,8 +18,9 @@ do
         echo $chainProbableDaemon
         daemonPid=$(ps aux | grep "$chainProbableDaemon" | grep -v grep | awk ' { print $2 } ')
         if [ ! -z "$daemonPid" ]; then
+		echo "Found old daemon pid $daemonPid, killing process..."
                 kill $daemonPid
-                #Hopefully enough, else loop and wait until gone
+                #Hopefully enough, else TODO: loop and wait until gone
                 sleep 10s
         fi
         sudo su - "$userOfChain" -c "source $availableChain && $chainProbableExec start farmer"
